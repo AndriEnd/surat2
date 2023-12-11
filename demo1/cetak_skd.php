2b564cae3daf06f935964abad1a2243818557340
AndriEnd <ndrian1710@gmail.com>
2023-12-11 22:24:31 +0700

    update penyesuaian view dan cetak surat

--- demo1/cetak_skd.php
+++ demo1/cetak_skd.php
@@ -1,91 +1,95 @@
-<?php include '../konek.php';?>
+<?php include '../konek.php'; ?>
 <?php
-	if(isset($_GET['id_request_skd'])){
-		$id=$_GET['id_request_skd'];
-		$sql = "SELECT * FROM data_request_skd natural join data_user WHERE id_request_skd='$id'";
-		$query = mysqli_query($konek,$sql);
-        $data = mysqli_fetch_array($query,MYSQLI_BOTH);
-        $nik = $data['nik'];
-		$nama = $data['nama'];
-		$tempat = $data['tempat_lahir'];
-        $tgl = $data['tanggal_lahir'];
-        $tgl2 = $data['tanggal_request'];
-        $format1 = date('Y', strtotime($tgl2));
-        $format2 = date('d-m-Y', strtotime($tgl));
-        $format3 = date('d F Y', strtotime($tgl2));
-		$agama = $data['agama'];
-		$jekel = $data['jekel'];
-		$nama = $data['nama'];
-		$alamat = $data['alamat'];
-		$status_warga = $data['status_warga'];
-        $request = $data['request'];
-        $keperluan = $data['keperluan'];
-        $acc = $data['acc'];
-        $format4 = date('d F Y', strtotime($acc));
-	}
+if (isset($_GET['id_request_skd'])) {
+    $id = $_GET['id_request_skd'];
+    $sql = "SELECT * FROM data_request_skd natural join data_user WHERE id_request_skd='$id'";
+    $query = mysqli_query($konek, $sql);
+    $data = mysqli_fetch_array($query, MYSQLI_BOTH);
+    $nik = $data['nik'];
+    $nama = $data['nama'];
+    $tempat = $data['tempat_lahir'];
+    $tgl = $data['tanggal_lahir'];
+    $tgl2 = $data['tanggal_request'];
+    $format1 = date('Y', strtotime($tgl2));
+    $format2 = date('d-m-Y', strtotime($tgl));
+    $format3 = date('d F Y', strtotime($tgl2));
+    $agama = $data['agama'];
+    $jekel = $data['jekel'];
+    $nama = $data['nama'];
+    $alamat = $data['alamat'];
+    $status_warga = $data['status_warga'];
+    $request = $data['request'];
+    $keperluan = $data['keperluan'];
+    $acc = $data['acc'];
+    $format4 = date('d F Y', strtotime($acc));
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CETAK SKD</title>
 </head>
+
 <body>
 
     <table border="0" align="center">
         <tr>
-        <td><img src="img/kudus.png" width="70" height="87" alt=""></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+            <td><img src="img/logo1.png" width="70" height="87" alt=""></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
             <td>
                 <center>
-                    <font size="4">PEMERINTAHAN KABUPATEN KUDUS</font><br>
-                    <font size="4">KECAMATAN KOTA KUDUS</font><br>
-                    <font size="5"><b>KELURAHAN WERGU WETAN</b></font><br>
-                    <font size="2"><i>JL. Loram No.9 Kudus 59318</i></font><br>
+                    <font size="4">PEMERINTAHAN KABUPATEN LAMPUNG TENGAH</font><br>
+                    <font size="4">KECAMATAN SEPUTI BANYAK</font><br>
+                    <font size="5"><b>>KELURAHAN SUMBER BAHAGIA</b></font><br>
+                    <font size="2"><i>JL.SOLO NO 1 , 34156 </i></font><br>
                 </center>
             </td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
         </tr>
         <tr>
-            <td colspan="45"><hr color="black"></td>
+            <td colspan="45">
+                <hr color="black">
+            </td>
         </tr>
     </table>
     <br>
@@ -95,7 +99,7 @@
                 <center>
                     <font size="4"><b>SURAT KETERANGAN / PENGANTAR</b></font><br>
                     <hr style="margin:0px" color="black">
-                    <span>Nomor : 045.2 / <?php echo $id;?> / 29.07.05</span>
+                    <span>Nomor : 045.2 / <?php echo $id; ?> / 29.07.05</span>
                 </center>
             </td>
         </tr>
@@ -105,7 +109,7 @@
     <table border="0" align="center">
         <tr>
             <td>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Yang bertanda tangan di bawah ini Lurah Wergu Wetan Kabupaten Kota <br> Kudus, Menerangkan bahwa :
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Yang bertanda tangan di bawah ini Lurah Sumber Bahagia Kecamatan Seputi Banyak <br> Lampung Tengah, Menerangkan bahwa :
             </td>
         </tr>
     </table>
@@ -114,52 +118,52 @@
         <tr>
             <td>Nama</td>
             <td>:</td>
-            <td><?php echo $nama;?></td>
+            <td><?php echo $nama; ?></td>
         </tr>
         <tr>
             <td>TTL</td>
             <td>:</td>
-            <td><?php echo $tempat.", ".$format2;?></td>
+            <td><?php echo $tempat . ", " . $format2; ?></td>
         </tr>
         <tr>
             <td>Jenis Kelamin</td>
             <td>:</td>
-            <td><?php echo $jekel;?></td>
+            <td><?php echo $jekel; ?></td>
         </tr>
         <tr>
             <td>Agama</td>
             <td>:</td>
-            <td><?php echo $agama;?></td>
+            <td><?php echo $agama; ?></td>
         </tr>
         <tr>
             <td>Status Warga</td>
             <td>:</td>
-            <td><?php echo $status_warga;?></td>
+            <td><?php echo $status_warga; ?></td>
         </tr>
         <tr>
             <td>No. NIK</td>
             <td>:</td>
-            <td><?php echo $nik;?></td>
+            <td><?php echo $nik; ?></td>
         </tr>
         <tr>
             <td>Alamat</td>
             <td>:</td>
-            <td><?php echo $alamat;?></td>
+            <td><?php echo $alamat; ?></td>
         </tr>
         <tr>
             <td>Keperluan</td>
             <td>:</td>
-            <td><?php echo $keperluan;?></td>
+            <td><?php echo $keperluan; ?></td>
         </tr>
         <tr>
             <td>Keterangan</td>
             <td>:</td>
             <?php
-                if($request=="DOMISILI"){
-                    $request="Surat Keterangan Domisili";
-                }
+            if ($request == "DOMISILI") {
+                $request = "Surat Keterangan Domisili";
+            }
             ?>
-            <td><?php echo $request;?></td>
+            <td><?php echo $request; ?></td>
         </tr>
     </table>
     <br>
@@ -176,12 +180,12 @@
         <tr>
             <th></th>
             <th width="100px"></th>
-            <th>Kudus, <?php echo $format4;?></th>
+            <th>Lampung Tengah, <?php echo $format4; ?></th>
         </tr>
         <tr>
-            <td>Tanda tangan <br> Yang bersangkutan </td>
+            <td>Tanda Tangan <br> Yang Bersangkutan </td>
             <td></td>
-            <td>Lurah Wergu Wetan</td>
+            <td>Lurah Sumber Bahagia </td>
         </tr>
         <tr>
             <td rowspan="15"></td>
@@ -223,25 +227,29 @@
         </tr>
         <tr>
             <td></td>
-        </tr><tr>
-            <td></td>
-        </tr><tr>
-            <td></td>
-        </tr><tr>
+        </tr>
+        <tr>
             <td></td>
         </tr>
         <tr>
-            <td><b style="text-transform:uppercase"><u>(<?php echo $nama;?>)</u></b></td>
             <td></td>
-            <td><b><u>(AGUS SUPRIYANTO)</u></b></td>
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+        <tr>
+            <td><b style="text-transform:uppercase"><u>(<?php echo $nama; ?>)</u></b></td>
+            <td></td>
+            <td><b><u>(Burhan Suburhan)</u></b></td>
         </tr>
     </table>
 
 
 
-    
+
 </body>
+
 </html>
-        <script>
-            window.print();
-        </script>
+<script>
+    window.print();
+</script>
--- demo1/cetak_sktm.php
+++ demo1/cetak_sktm.php
@@ -1,91 +1,95 @@
-<?php include '../konek.php';?>
+<?php include '../konek.php'; ?>
 <?php
-	if(isset($_GET['id_request_sktm'])){
-		$id=$_GET['id_request_sktm'];
-		$sql = "SELECT * FROM data_request_sktm natural join data_user WHERE id_request_sktm='$id'";
-		$query = mysqli_query($konek,$sql);
-        $data = mysqli_fetch_array($query,MYSQLI_BOTH);
-        $nik = $data['nik'];
-		$nama = $data['nama'];
-		$tempat = $data['tempat_lahir'];
-        $tgl = $data['tanggal_lahir'];
-        $tgl2 = $data['tanggal_request'];
-        $format2 = date('Y', strtotime($tgl2));
-        $format1 = date('d-m-Y', strtotime($tgl));
-        $format3 = date('d F Y', strtotime($tgl2));
-		$agama = $data['agama'];
-		$jekel = $data['jekel'];
-		$nama = $data['nama'];
-		$alamat = $data['alamat'];
-		$status_warga = $data['status_warga'];
-        $request = $data['request'];
-        $keperluan = $data['keperluan'];
-        $acc = $data['acc'];
-        $format4 = date('d F Y', strtotime($acc));
-	}
+if (isset($_GET['id_request_sktm'])) {
+    $id = $_GET['id_request_sktm'];
+    $sql = "SELECT * FROM data_request_sktm natural join data_user WHERE id_request_sktm='$id'";
+    $query = mysqli_query($konek, $sql);
+    $data = mysqli_fetch_array($query, MYSQLI_BOTH);
+    $nik = $data['nik'];
+    $nama = $data['nama'];
+    $tempat = $data['tempat_lahir'];
+    $tgl = $data['tanggal_lahir'];
+    $tgl2 = $data['tanggal_request'];
+    $format2 = date('Y', strtotime($tgl2));
+    $format1 = date('d-m-Y', strtotime($tgl));
+    $format3 = date('d F Y', strtotime($tgl2));
+    $agama = $data['agama'];
+    $jekel = $data['jekel'];
+    $nama = $data['nama'];
+    $alamat = $data['alamat'];
+    $status_warga = $data['status_warga'];
+    $request = $data['request'];
+    $keperluan = $data['keperluan'];
+    $acc = $data['acc'];
+    $format4 = date('d F Y', strtotime($acc));
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CETAK SKTM</title>
 </head>
+
 <body>
 
     <table border="0" align="center">
         <tr>
-        <td><img src="img/logo1.png" width="70" height="87" alt=""></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+            <td><img src="img/logo1.png" width="70" height="87" alt=""></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
             <td>
                 <center>
                     <font size="4">PEMERINTAHAN KABUPATEN LAMPUNG TENGAH</font><br>
                     <font size="4">KECAMATAN SEPUTI BANYAK</font><br>
                     <font size="5"><b>KELURAHAN SUMBER BAHAGIA</b></font><br>
-                    <font size="2"><i>JL.SOLO NO 1 , 34156 </i></font><br> 
+                    <font size="2"><i>JL.SOLO NO 1 , 34156 </i></font><br>
                 </center>
             </td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
         </tr>
         <tr>
-            <td colspan="45"><hr color="black"></td>
+            <td colspan="45">
+                <hr color="black">
+            </td>
         </tr>
     </table>
     <br>
@@ -95,7 +99,7 @@
                 <center>
                     <font size="4"><b>SURAT KETERANGAN / PENGANTAR</b></font><br>
                     <hr style="margin:0px" color="black">
-                    <span>Nomor : 045.2 / <?php echo $id;?> / 29.07.05 </span>
+                    <span>Nomor : 045.2 / <?php echo $id; ?> / 29.07.05 </span>
                 </center>
             </td>
         </tr>
@@ -105,7 +109,7 @@
     <table border="0" align="center">
         <tr>
             <td>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Yang bertanda tangan di bawah ini Lurah Sumber Bahagia Kecamatan Seputi Banyak  <br> Lampung Tengah, Menerangkan bahwa :
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Yang bertanda tangan di bawah ini Lurah Sumber Bahagia Kecamatan Seputi Banyak <br> Lampung Tengah, Menerangkan bahwa :
             </td>
         </tr>
     </table>
@@ -114,52 +118,52 @@
         <tr>
             <td>Nama</td>
             <td>:</td>
-            <td><?php echo $nama;?></td>
+            <td><?php echo $nama; ?></td>
         </tr>
         <tr>
             <td>Tempat, tanggal lahir</td>
             <td>:</td>
-            <td><?php echo $tempat.", ".$format1;?></td>
+            <td><?php echo $tempat . ", " . $format1; ?></td>
         </tr>
         <tr>
             <td>Jenis Kelamin</td>
             <td>:</td>
-            <td><?php echo $jekel;?></td>
+            <td><?php echo $jekel; ?></td>
         </tr>
         <tr>
             <td>Agama</td>
             <td>:</td>
-            <td><?php echo $agama;?></td>
+            <td><?php echo $agama; ?></td>
         </tr>
         <tr>
             <td>Status Warga</td>
             <td>:</td>
-            <td><?php echo $status_warga;?></td>
+            <td><?php echo $status_warga; ?></td>
         </tr>
         <tr>
             <td>No. NIK</td>
             <td>:</td>
-            <td><?php echo $nik;?></td>
+            <td><?php echo $nik; ?></td>
         </tr>
         <tr>
             <td>Alamat</td>
             <td>:</td>
-            <td><?php echo $alamat;?></td>
+            <td><?php echo $alamat; ?></td>
         </tr>
         <tr>
             <td>Keperluan</td>
             <td>:</td>
-            <td><?php echo $keperluan;?></td>
+            <td><?php echo $keperluan; ?></td>
         </tr>
         <tr>
             <td>Keterangan</td>
             <td>:</td>
             <?php
-                if($request=="TIDAK MAMPU"){
-                    $request="Surat Keterangan Tidak Mampu";
-                }
+            if ($request == "TIDAK MAMPU") {
+                $request = "Surat Keterangan Tidak Mampu";
+            }
             ?>
-            <td><?php echo $request;?></td>
+            <td><?php echo $request; ?></td>
         </tr>
     </table>
     <br>
@@ -176,10 +180,10 @@
         <tr>
             <th></th>
             <th width="100px"></th>
-            <th>Lampung Tengah, <?php echo $format4;?></th>
+            <th>Lampung Tengah, <?php echo $format4; ?></th>
         </tr>
         <tr>
-            <td>Tanda tangan <br> Yang bersangkutan </td>
+            <td>Tanda Tangan <br> Yang Bersangkutan </td>
             <td></td>
             <td>Lurah Sumber Bahagia</td>
         </tr>
@@ -223,15 +227,18 @@
         </tr>
         <tr>
             <td></td>
-        </tr><tr>
-            <td></td>
-        </tr><tr>
-            <td></td>
-        </tr><tr>
+        </tr>
+        <tr>
             <td></td>
         </tr>
         <tr>
-            <td><b style="text-transform:uppercase"><u>(<?php echo $nama;?>)</u></b></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+        <tr>
+            <td><b style="text-transform:uppercase"><u>(<?php echo $nama; ?>)</u></b></td>
             <td></td>
             <td><b><u>(Burhan Suburhan)</u></b></td>
         </tr>
@@ -239,9 +246,10 @@
 
 
 
-    
+
 </body>
+
 </html>
-        <script>
-            window.print();
-        </script>
+<script>
+    window.print();
+</script>
