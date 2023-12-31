15fc3eba5158b852aea69dff2f020a47e958e4c7
AndriEnd <ndrian1710@gmail.com>
2024-01-01 00:18:30 +0700

    update malem tahun baru

--- konek.php
+++ konek.php
@@ -3,6 +3,6 @@ date_default_timezone_set('Asia/Jakarta');
 $hostname = 'localhost:3307';
 $username = 'root';
 $password = 'MSI123';
-$database = 'surat2new';
+$database = 'db_surat';
 
 $konek = mysqli_connect($hostname, $username, $password, $database);
